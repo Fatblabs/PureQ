3f22987d3348a069f126942734a1f218cf0cfbed
Fatblabs <stuffducky@gmail.com>
2026-05-11 12:17:26 -0700

    Initial Commit

---
+++ PureQ/PureQApp.swift
@@ -0,0 +1,17 @@
+//
+//  PureQApp.swift
+//  PureQ
+//
+//  Created by Sean Nguyen on 5/11/26.
+//
+
+import SwiftUI
+
+@main
+struct PureQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
